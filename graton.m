53a18941aae60af5bf27b712515b1613d71a514f
yoni <yoni.melman@gmail.com>
2015-05-25 15:11:34 +0300

    fixed finding columns in the excel file

--- graton.m
+++ graton.m
@@ -9,40 +9,45 @@ disp('Fill in the following questions');
 
 % Subject
 columns.subject.name = input('Subject Col: ','s');
-columns.subject.col_num = find(ismember(columns.subject.name,header));
+columns.subject.col_num = find(ismember(header,columns.subject.name));
 columns.subject.conditions = unique(data(:,columns.subject.col_num));
 %ACC
 columns.acc.name = input('Accurace Col: ','s');
-columns.acc.col_num = find(ismember(columns.acc.name,header));
+columns.acc.col_num = find(ismember(header,columns.acc.name));
 %RT
 columns.RT.name = input('RT Col: ','s');
-columns.RT.col_num = find(ismember(columns.RT.name,header));
+columns.RT.col_num = find(ismember(header,columns.RT.name));
 %BLOCK
-columns.block.name = input('RT Col: ','s');
-columns.block.col_num = find(ismember(columns.block.name,header));
-columns.block.conditions = unique(data(:,columns.block.col_num));
+% columns.block.name = input('RT Col: ','s');
+% columns.block.col_num = find(ismember(header,columns.block.name));
+% columns.block.conditions = unique(data(:,columns.block.col_num));
 
 %DESIGN
 if strcmp(input('within / between subjects Design?: ','s'),'within')
     columns.condition.name = input('Condition Col: ','s');
-    columns.condition.col_num = find(ismember(columns.condition.name,header));
+    columns.condition.col_num = find(ismember(header,columns.condition.name));
     columns.condition.conditions = unique(data(:,columns.condition.col_num));
 end
 %VARIABLES
 vars = strsplit(input('Variables: (enter with spaces): ','s'));
 for i = 1:length(vars)
     columns.vars(i).name = vars{i}
-    columns.vars(i).col_num = find(ismember(columns.vars(i).name,header));
+    columns.vars(i).col_num = find(ismember(header,columns.vars(i).name));
     columns.vars(i).conditions = unique(data(:,columns.vars(i).col_num));
 end
 
 if strcmp(input('handed response?: ','s'),'yes')
     columns.handed_resp.name = input('Handed Response Col: ','s');
-    columns.handed_resp.col_num = find(ismember(columns.handed_resp.name,header));
+    columns.handed_resp.col_num = find(ismember(header,columns.handed_resp.name));
     columns.handed_resp.conditions = unique(data(:,columns.handed_resp.col_num));
 
 end
 
 columns.congruity.name = input('Congruity Col: ','s');
-columns.congruity.col_num = find(ismember(columns.congruity.name,header))
+columns.congruity.col_num = find(ismember(header,columns.congruity.name))
 columns.congruity.conditions = unique(data(:,columns.congruity.col_num));
+
+
+%%
+
+
